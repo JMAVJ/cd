9f2681df4415389a2e29adb0b46ad75beda99b20
JMAVJ <josemiguel.velasquezj@gmail.com>
2020-11-24 22:56:06 -0300

    Add hello_world.py

---
+++ Python/Fundamentals/Fundamentals/hello_world.py
@@ -0,0 +1,14 @@
+print('Hola mundo')
+
+name = 'Jose'
+print('Hola ', name, '!')
+print('Hola ' + name + '!')
+
+num = 8
+print('Hola', num)
+print('Hola ' + str(num))
+
+food_one = 'Pizza'
+food_two = 'Sushi'
+
+print(f"Text... {food_one} more text {food_two}")
