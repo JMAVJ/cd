c6691bdf5342cd5036288acd7ebd3ab51ccd7a9e
JMAVJ <josemiguel.velasquezj@gmail.com>
2020-12-13 18:44:38 -0300

    Add chess board

---
+++ Python/Flask/Fundamentals/Chess/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'x': 8, 'y': 8})
+@app.route('/<int:x>', defaults={'y': 8})
+@app.route('/<int:x>/<int:y>')
+def chess(x, y):
+    return render_template('index.html', x=x, y=y)
+
+if __name__ == '__main__':
+    app.run(debug=True)
