7e56a169174f68d613068eac4ec84cfb8977ce69
JMAVJ <josemiguel.velasquezj@gmail.com>
2020-12-23 22:08:52 -0300

    Fix form validation

--- Python/Flask/Flask+MySQL/Form_Validation/app.py
+++ Python/Flask/Flask+MySQL/Form_Validation/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from mysqlconnection import *
 
 app = Flask(__name__)
+app.secret_key = 'key'
 
 # Views
 @app.route('/')
@@ -16,7 +17,6 @@ def user(id):
     }
     query = 'SELECT * FROM form WHERE id = %(id)s'
     result = mysql.query_db(query, data)
-    print(result)
     return render_template('user.html', data=result[0])
 
 
@@ -30,6 +30,18 @@ def send_form():
         'language': request.form['language'],
         'comment': request.form['comment'],
     }
+
+    for key in data:
+        if key == 'comment':
+            continue
+        if len(data[key]) < 1:
+            flash(f'You can not leave the {key} empty')
+            return redirect('/')
+    
+    if len(data['comment']) > 120:
+        flash('The comment must be less than 120 characters long')
+        return redirect('/')
+
     query = 'INSERT INTO `form` (name, location, language, comment) VALUES (%(name)s, %(location)s, %(language)s, %(comment)s)'
     id = mysql.query_db(query, data)
     return redirect(f'/user/{id}')
