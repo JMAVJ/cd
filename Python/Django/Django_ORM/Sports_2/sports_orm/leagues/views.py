d5ac590620f2867fe8e00658020e5fbcf1636029
JMAVJ <josemiguel.velasquezj@gmail.com>
2021-03-26 14:23:00 -0300

    Sports ORM II

--- Python/Django/Django_ORM/Sports_2/sports_orm/leagues/views.py
+++ Python/Django/Django_ORM/Sports_2/sports_orm/leagues/views.py
@@ -6,26 +6,18 @@ from . import team_maker
 
 
 def index(request):
-    print('===')
-    # print(Team.objects.filter(
-    #     league=League.objects.get(name='Transamerican Basketball Conference').id))
-    transamerican_basketball_players = Team.objects.filter(league=League.objects.get(
-        name='Transamerican Basketball Conference'))
-
-    # transamerican_basketball_players = []
-
-    # for team in transamerican_basketball_teams:
-    #     transamerican_basketball_players.append(
-    #         team.curr_players.values('first_name', 'last_name'))
-
-    print(transamerican_basketball_players)
-
     context = {
-        "teams_in_transamerican_football_conference": Team.objects.filter(league_id=League.objects.get(
-            name='Transamerican Football Players Conference').id),
-        "actual_players_in_philadelphia_showboats": Player.objects.filter(curr_team=Team.objects.get(
-            team_name='Showboats', location='Philadelphia')),
-        "transamerican_basketball_conference_players": transamerican_basketball_players,
+        'query_1': Team.objects.filter(league=League.objects.filter(name="Pacific Soccer Federation")),
+        'query_2': Team.objects.get(team_name="Boston Penguins", location="California").curr_players.all(),
+        'query_3': League.objects.get(name="International Collegiate Baseball Conference").teams.values('curr_players'),
+        'query_4': Player.objects.filter(last_name="López", curr_team__league__name="Amateur American Football Conference"),
+        'query_5': Team.objects.filter(league__name__contains="Football"),
+        'query_6': Team.objects.filter(curr_players__first_name="Sophia"),
+        'query_7': League.objects.filter(teams__curr_players__first_name="Sophia"),
+        'query_8': Player.objects.filter(last_name="Flores").exclude(curr_team_id__league__name="Washington Roughriders"),
+        'query_9': Player.objects.get(first_name="Samuel",  last_name="Evans").all_teams,
+        'query_10': Player.objects.filter(first_name="Samuel").filter(last_name="Barnes")[0].all_teams.values(),
+        'query_11': Player.objects.filter(all_teams__team_name="Vikings", all_teams__location="Wichita").exclude(curr_team__team_name="Vikings", curr_team__location="Wichita"),
     }
     return render(request, "leagues/index.html", context)
 
