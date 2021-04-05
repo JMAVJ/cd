b23a6a00a385339a3fc3b7a9b0557169a4a9bc2a
JMAVJ <josemiguel.velasquezj@gmail.com>
2021-04-05 12:43:12 -0400

    Fix calculateHypotenuse method

--- Java/Fundamentals/Fundamentals/pythagoras/Pythagoras.java
+++ Java/Fundamentals/Fundamentals/pythagoras/Pythagoras.java
@@ -2,7 +2,6 @@ import java.lang.Math;
 
 class Pythagoras {
     public static double calculateHypotenuse(int sideA, int sideB) {
-        double hypotenuse = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
-        return hypotenuse;
+        return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
     }
 }
