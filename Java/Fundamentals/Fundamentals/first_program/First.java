02aab779da96b79957cd50bc35e8c6f7560936af
JMAVJ <josemiguel.velasquezj@gmail.com>
2021-04-05 12:17:32 -0400

    Add first java program

---
+++ Java/Fundamentals/Fundamentals/first_program/First.java
@@ -0,0 +1,7 @@
+class First {
+    public static void main(String[] args) {
+        System.out.println("String 1");
+        System.out.println("String 2");
+        System.out.println("String 3");
+    }
+}
