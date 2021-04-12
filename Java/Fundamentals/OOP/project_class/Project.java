11f70e4470f5f943910addec552e6202d210c6e2
JMAVJ <josemiguel.velasquezj@gmail.com>
2021-04-11 20:46:22 -0400

    Add Project Class

--- Java/Fundamentals/OOP/project_class/Project.java
+++ Java/Fundamentals/OOP/project_class/Project.java
@@ -1,6 +1,7 @@
 public class Project {
     String name;
     String description;
+    double initialCost;
 
     public Project() {
         this.name = "Name not set yet";
@@ -10,14 +11,46 @@ public class Project {
     public Project(String name) {
         this.name = name;
         this.description = "Description not set yet";
+        this.initialCost = 0.0;
     }
-
+    
     public Project(String name, String description) {
         this.name = name;
         this.description = description;
+        this.initialCost = 0.0;
+    }
+    
+    public Project(String name, String description, double initialCost) {
+        this.name = name;
+        this.description = description;
+        this.initialCost = initialCost;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
     }
 
-    public void setName(String name) {
-        
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setInitialCost(double initialCost) {
+        this.initialCost = initialCost;
+    }
+
+    public double getInitialCost() {
+        return this.initialCost;
+    }
+
+    public String toString() {
+        return this.name + "(" + this.initialCost + ")" + " : " + this.description;
     }
 }
---
+++ Java/Fundamentals/OOP/project_class/ProjectTest.java
@@ -0,0 +1,6 @@
+public class ProjectTest {
+    public static void main(String[] args) {
+        Project p1 = new Project("Test", "Desc", 50.0);
+        System.out.println(p1);
+    }
+}
