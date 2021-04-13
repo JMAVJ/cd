41d47657ef7abf48c0480b042aad4eeaf7399303
JMAVJ <josemiguel.velasquezj@gmail.com>
2021-04-12 20:51:46 -0400

    Add human1

---
+++ Java/Fundamentals/OOP/human1/human1/src/testing/HumanTest.java
@@ -0,0 +1,14 @@
+package testing;
+import characters.Human;
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		Human h1 = new Human();
+		Human h2 = new Human();
+		System.out.println(h2.getHealth());
+		h1.attack(h2);
+		System.out.println(h2.getHealth());
+	}
+
+}
