2d2913c077d92b3ef681831e6a6d0725a99c2cd6
JMAVJ <josemiguel.velasquezj@gmail.com>
2021-06-24 17:54:02 -0400

    Add ninja

---
+++ MERN/Javascript/OOP/ninja/ninja.js
@@ -0,0 +1,26 @@
+class Ninja {
+  constructor(name, health, speed=3, strength=3) {
+    this.name = name
+    this.health = health
+    this.speed = speed
+    this.strength = strength
+  }
+
+  sayName() {
+    console.log(this.name)
+  }
+
+  showStats() {
+    console.log(`Name: ${this.name}`)
+    console.log(`Strength: ${this.strength}`)
+    console.log(`Speed: ${this.speed}`)
+    console.log(`Health: ${this.health}`)
+  }
+
+  drinkSake() {
+    this.health += 10
+  }
+}
+
+n = new Ninja('ninja1', 100)
+n.showStats()
