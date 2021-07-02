120697d40f6b2ecc74fab14533dc601317d9c42f
JMAVJ <josemiguel.velasquezj@gmail.com>
2021-07-01 21:59:33 -0400

    add first-jsx

---
+++ MERN/React/Intro/first-jsx/src/components/HelloWorld.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <ul>
+        <li>task 1</li>
+        <li>task 2</li>
+        <li>task 3</li>
+        <li>task 4</li>
+      </ul>
+    </div>
+  )
+}
+
+export default HelloWorld
