9a510b85405b9580f3fa69b8a71ce116fcccb55e
JMAVJ <josemiguel.velasquezj@gmail.com>
2021-07-04 14:37:34 -0400

    add pokemon-axios

---
+++ MERN/React/API/pokemon-axios/src/components/Pokemon.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Pokemon = () => {
+  const [data, setData] = useState([]);
+
+  const handleClick = () => {
+    const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=100';
+    axios
+      .get(API_URL)
+      .then(({ data: { results } }) => {
+        setData(results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <button onClick={() => handleClick()}>Fetch Pokemon</button>
+      {data.map((pokemon, index) => (
+        <h1 key={index}>{pokemon.name}</h1>
+      ))}
+    </>
+  );
+};
+
+export default Pokemon;
